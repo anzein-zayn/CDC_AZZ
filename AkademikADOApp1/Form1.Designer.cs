e6c2fb93d83d39318495922aea906b6f683be9cc
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2026-03-10 11:47:39 +0700

    Memberi Nama Pada Label

--- AkademikADOApp1/Form1.Designer.cs
+++ AkademikADOApp1/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.btnConnect = new System.Windows.Forms.Button();
             this.btnDisconnected = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnConnect
@@ -52,22 +52,22 @@
             this.btnDisconnected.Text = "Disconneted Database";
             this.btnDisconnected.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // lblStatus
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(182, 79);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(182, 79);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 16);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Status";
+            this.lblStatus.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
             this.Controls.Add(this.btnDisconnected);
             this.Controls.Add(this.btnConnect);
             this.Name = "Form1";
@@ -81,7 +81,7 @@
 
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.Button btnDisconnected;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
