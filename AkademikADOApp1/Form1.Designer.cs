db4cace97fdfe5d106390d6e1cf8ed7defbc5a89
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2026-03-10 11:36:20 +0700

    Memberi Nama pada Button 2 atau Disconnected

--- AkademikADOApp1/Form1.Designer.cs
+++ AkademikADOApp1/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.btnConnect = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnDisconnected = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnConnect
@@ -42,21 +42,21 @@
             this.btnConnect.UseVisualStyleBackColor = true;
             this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
-            // button1
+            // btnDisconnected
             // 
-            this.button1.Location = new System.Drawing.Point(510, 361);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnDisconnected.Location = new System.Drawing.Point(467, 361);
+            this.btnDisconnected.Name = "btnDisconnected";
+            this.btnDisconnected.Size = new System.Drawing.Size(172, 23);
+            this.btnDisconnected.TabIndex = 1;
+            this.btnDisconnected.Text = "Disconneted Database";
+            this.btnDisconnected.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDisconnected);
             this.Controls.Add(this.btnConnect);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -67,7 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnConnect;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDisconnected;
     }
 }
 
