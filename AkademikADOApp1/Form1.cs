ec88f3ebd75e71615c1a275212035e23c5e4704b
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2026-03-10 11:33:55 +0700

    Memberi Nama pada button 1

--- AkademikADOApp1/Form1.cs
+++ AkademikADOApp1/Form1.cs
@@ -16,5 +16,10 @@ namespace AkademikADOApp1
         {
             InitializeComponent();
         }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
